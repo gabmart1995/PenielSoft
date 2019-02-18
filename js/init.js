9e8611da305b4c0734ddebcbd695e4dfd07978ed
Gabriel Martínez <gabmart1995@gmail.com>
2019-02-18 12:14:52 -0400

    slider terminado, dropdown y scrollspy Version 1.0

--- js/init.js
+++ js/init.js
@@ -1,21 +1,35 @@
-// Inicializacion de componentes de Materialize en JavaScript
+//Inicializacion de componentes de Materialize en JavaScript
 document.addEventListener('DOMContentLoaded', function() {
-    //menu responsivo lateral
+    //sidenav
     var elemento1 = document.querySelectorAll('.sidenav');
     var instancia1 = M.Sidenav.init(elemento1);
-    //contenedor parallax
+    //parallax
     var elemento2 = document.querySelectorAll('.parallax');
     var instancia2 = M.Parallax.init(elemento2);
-    //menu collapsable
+    //collapsible
     var elemento3 = document.querySelectorAll('.collapsible');
     var instancia3 = M.Collapsible.init(elemento3);
-    //ventana modal
+    //modal
     var elemento4 = document.querySelectorAll('.modal');
     var instancia4 = M.Modal.init(elemento4);
     //floating button
     var elemento5 = document.querySelectorAll('.fixed-action-btn');
     var instancia5 = M.FloatingActionButton.init(elemento5, {
+        //options
         hoverEnabled: false
     });
     //galeria slider
+    var elemento6 = document.querySelectorAll('.slider');
+    var instancia6 = M.Slider.init(elemento6, {
+        //options
+        indicators: false,
+        interval: 3000, //seconds
+        height: 425
+    });
+    //dropdown
+    var elemento7 = document.querySelectorAll('.dropdown-trigger');
+    var instancia7 = M.Dropdown.init(elemento7);
+    //scrollspy
+    var elemento8 = document.querySelectorAll('.scrollspy');
+    var instancia8 = M.ScrollSpy.init(elemento8);
 });
